a7c3c084b0dd44293684fdeb94fcd2102df2e9e6
Sondra Miller <sondra.n.miller@gmail.com>
2020-08-27 10:42:32 -0400

    subscription attempt 1 mixed w 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import TodoList from './TodoList';
-import { QueryRenderer } from 'react-relay';
+import { QueryRenderer, requestSubscription } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 import RelayEnvironment from './RelayEnvironment';
+import { ConnectionHandler } from 'relay-runtime';
 
 const AppAllTodosQuery = graphql`
   query AppAllTodosQuery {
@@ -11,22 +12,71 @@ const AppAllTodosQuery = graphql`
   }
 `
 
-
-function App(props) {
-  return (
-    <QueryRenderer
-      environment={RelayEnvironment}
-      query={AppAllTodosQuery}
-      render={({error, props}) => {
-        if (error) {
-          return <div>{error.message}</div>
-        } else if (props) {
-          return <TodoList viewer={props} />
+class App extends React.Component {
+  componentDidMount () {
+    const subscriptionConfig = {
+      // #1
+      subscription: graphql`
+        subscription Apptodo_app_todos_connectionSubscription {
+          todo_app_todos_connection {
+            edges {
+              node {
+                complete
+                id
+                text
+                user_id
+                user {
+                  id
+                  name
+                  todos_aggregate {
+                    aggregate {
+                      count(distinct: true)
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
-        return <div>Loading</div>
-      }}
-    />
-  );
-}
+      `,
+      onCompleted: data => console.log(data),
+      onError: error => console.error(error),
+      // #2
+      updater: store => {
+        const newRecord = store.getRootField('todo_app_todos_connection').getLinkedRecord('node')
+        // #2A
+        const conn = ConnectionHandler.getConnection(
+          store.getRoot(),
+          'TodoList_todo_app_todos_connection'
+        )
+        // #2B
+        const edge = ConnectionHandler.createEdge(store, conn, newRecord, 'edge')
+        ConnectionHandler.insertEdgeAfter(conn, edge)
+      }
+    }
+  
+    this.subscription = requestSubscription(RelayEnvironment, subscriptionConfig)
+  }
+  
+  componentWillUnmount () {
+    this.subscription.dispose()
+  }
 
+  render() {
+      return (
+        <QueryRenderer
+          environment={RelayEnvironment}
+          query={AppAllTodosQuery}
+          render={({error, props}) => {
+            if (error) {
+              return <div>{error.message}</div>
+            } else if (props) {
+              return <TodoList viewer={props} />
+            }
+            return <div>Loading</div>
+          }}
+        />
+      );
+  }
+}
 export default App;
--- src/RelayEnvironment.js
+++ src/RelayEnvironment.js
@@ -1,12 +1,43 @@
 import fetchGraphQL from './fetchGraphQL';
 import { Environment, Network, Store, RecordSource } from 'relay-runtime';
+import { execute } from 'apollo-link';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { WebSocketLink } from 'apollo-link-ws'
 
 async function fetchRelay(params, variables) {
     console.log(`fetching query ${params.name} with ${JSON.stringify(variables)}`);
     return fetchGraphQL(params.text, variables);
 }
+const REACT_APP_RELAY_SUBSCRIPTION_ENDPOINT = "ws://localhost:8080/v1beta1/relay"
+const subscriptionClientOld = new SubscriptionClient(REACT_APP_RELAY_SUBSCRIPTION_ENDPOINT, {
+    reconnect: true,
+    timeout: 30000
+});
+
+function setupSubscription(
+    config,
+    variables,
+    cacheConfig,
+    observer,
+  ) {
+    const query = config.text
+  
+    const subscriptionClient = new SubscriptionClient(REACT_APP_RELAY_SUBSCRIPTION_ENDPOINT, {reconnect: true})
+    const id = subscriptionClient.subscribe({query, variables}, (error, result) => {
+      observer.onNext({data: result})
+    })
+  }
+
+const subscriptionLink = new WebSocketLink(subscriptionClientOld)
+
+// Prepare network layer from apollo-link for graphql subscriptions
+const networkSubscriptions = (operation, variables) =>
+  execute(subscriptionLink, {
+    query: operation.text,
+    variables
+  })
 
 export default new Environment({
-    network: Network.create(fetchRelay),
+    network: Network.create(fetchRelay, setupSubscription),
     store: new Store(new RecordSource())
 });
