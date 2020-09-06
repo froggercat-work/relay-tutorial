eb7ae09879fdde7d1f05293ff94dc92e33b22c5b
Sondra Miller <sondra.n.miller@gmail.com>
2020-09-06 14:20:11 -0400

    ✨ Updating is aliiiiiive

--- src/components/Checkbox/Checkbox.component.js
+++ src/components/Checkbox/Checkbox.component.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Checkbox.css';
+import TodoStatusMutations from '../../mutations/TodoStatusMutations';
 
 export default class Checkbox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { ...this.props };
-    }
-
     handleClick = () => {
         console.log("clicked this", this);
+        const {todo: { complete, todo_id } } = this.props;
+        console.log("Preparing to run updates with", "todo_id:", todo_id, "complete:", complete)
+        TodoStatusMutations(todo_id, !complete, () => console.log("Mutation complete"));
     }
 
     render() {
